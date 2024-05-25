3aea000b1b45c3e5808b4e89b2a6748903acd4c1
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-05-25 15:06:23 +0900

    Initial commit

---
+++ Assets/Scripts/Behaviour/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    private PlayerController playerController;
+
+    private Vector3 moveDirection;
+    [SerializeField] private float moveSpeed;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerController = GetComponent<PlayerController>();
+    }
+    void Start()
+    {
+        playerController.OnMoveEvent += Move;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(moveDirection);
+    }
+
+    private void Move(Vector2 dir)
+    {
+        Vector3 direction = transform.forward * dir.y + transform.right * dir.x;
+        direction *= moveSpeed;
+        direction.y = playerRigidbody.velocity.y;
+        moveDirection = direction;
+    }
+    private void ApplyMovement(Vector3 dir)
+    {
+        playerRigidbody.velocity = dir;
+    }
+}
---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    Vector2 curMovementInput;
+
+
+    Vector2 moveDirection;
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if(context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+        CallMoveEvent(curMovementInput);
+    }
+
+    private void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+
+    private void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+
+}
+
